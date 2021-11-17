2ebe21606c53bc2b9fe7597699d07e72ba9f69ba
Leo Bartnik <leo.bartnik@biotronik.com>
2021-11-17 13:15:18 -0800

    updated

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // globals
-window.onerror = function(errMessage) { alert('error'); }
+window.onerror = function(errMessage) { alert('error: ' + errMessage); }
 var canvas = document.getElementById("outside");
 var ctx = canvas.getContext("2d");
 var intensityPath = [1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9];
