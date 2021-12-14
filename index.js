036d7849ade7b388059a3cb78cfb31e400434cd6
Leo Bartnik <leo.bartnik@biotronik.com>
2021-12-14 15:19:32 -0800

    updated

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 "use strict";
 
 // globals
-window.onerror = function(errMessage) {
+window.onerror = function (errMessage) {
   alert("Error: " + errMessage);
 }
 
 var canvas = document.getElementById("outside");
+canvas.onclick = function () { canvasClick(); }
 var ctx = canvas.getContext("2d");
 var intensityPath = getIntensityPath();
 var lights = [];
@@ -22,42 +23,46 @@ var lightCount = parseInt(getParam("lightCount", 400), 10);
 var intervalGap = parseInt(getParam("intervalGap", 100), 10);
 var intervalGapCloser = parseInt(getParam("intervalGapCloser", 100), 10);
 var flow = getParam("flow", "false");
-
 var timeout = 5000;
+
+// TODO: Make different intensity path easing functions
+// TODO: Make mutation (+1 on interval?  random color change?) and make it configurable //var allowMutation = false;
+// TODO: Make random vs. ordered toggleable?  I.e. assign a grid position to all lights as well as a random position?
 
 // TODO: Use onerror method to deal with browsers that don't support URLSearchParams?  I.e. declare vars outside,
 //       attempt to read, onerror set values?
-// TODO: In the alternative, parse console.log("search: " + window.location.search);
-// TODO: Make mutation (+1 on interval?  random color change?) and make it configurable //var allowMutation = false;
-// TODO: Make random vs. ordered toggleable?  I.e. assign a grid position to all lights as well as a random position?
+// TODO: In the alternative, parse using an older approach that is more-browser compatible, eg. console.log("search: " + window.location.search);
 
 // main
 function run() {
 
   lights = [];
+
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
   if (random === "true") {
-    for (var i=0; i<lightCount; i++) {
-      var xPos = getRandomInt(margin, canvas.width - margin*2);
-      var yPos = getRandomInt(margin, canvas.height - margin*2);
+    for (var i = 0; i < lightCount; i++) {
+      var xPos = getRandomInt(margin, canvas.width - margin * 2);
+      var yPos = getRandomInt(margin, canvas.height - margin * 2);
       var light = lightModel(xPos, yPos, radius);
       lights.push(light);
       drawLight(light);
     }
   } else {
     var splitter = Math.sqrt(lightCount);
-    var widthSpace = canvas.width/splitter;
-    var heightSpace = canvas.height/splitter;
-    for (var x=0; x<lightCount/splitter; x++) {
-      for (var y=0; y<lightCount/splitter; y++) {
-        var xPos = x*widthSpace + margin;
-        var yPos = y*heightSpace + margin;
+    var widthSpace = canvas.width / splitter;
+    var heightSpace = canvas.height / splitter;
+    for (var x = 0; x < lightCount / splitter; x++) {
+      for (var y = 0; y < lightCount / splitter; y++) {
+        var xPos = x * widthSpace + margin;
+        var yPos = y * heightSpace + margin;
         var light = lightModel(xPos, yPos, radius);
         lights.push(light);
         drawLight(light);
       }
     }
   }
+
   lightTimer = setInterval(updateLights, 100);
 }
 
@@ -66,28 +71,28 @@ function lightModel(x, y, radius) {
   var light = {
     "interval": getRandomInt(10, 1100),
     "ambientLight": 1,
-    "ambientLightIntensity": function() { return "rgba(0,0,0," + (1-this.ambientLight) + ")" },
-     "intensity": function () { return intensityPath[this.intensityPathIndex]; },
-     "intensityPathIndex": getRandomInt(0, intensityPath.length - 1),
-     "x": x,
-     "y": y,
-     "r": getRandomInt(0, 255),
-     "g": getRandomInt(0, 255),
-     "b": getRandomInt(0, 255),
-     "a": function() { return (1 - this.intensity()); },
-     "color": function() { return "rgba(" + this.r + ", " + this.g + ", " + this.b + ", " + this.a() + ")"; },
-     "radius": radius
+    "ambientLightIntensity": function () { return "rgba(0,0,0," + (1 - this.ambientLight) + ")" },
+    "intensity": function () { return intensityPath[this.intensityPathIndex]; },
+    "intensityPathIndex": getRandomInt(0, intensityPath.length - 1),
+    "x": x,
+    "y": y,
+    "r": getRandomInt(0, 255),
+    "g": getRandomInt(0, 255),
+    "b": getRandomInt(0, 255),
+    "a": function () { return (1 - this.intensity()); },
+    "color": function () { return "rgba(" + this.r + ", " + this.g + ", " + this.b + ", " + this.a() + ")"; },
+    "radius": radius
   };
   return light;
 }
 
 function drawLight(light) {
-  var g = ctx.createRadialGradient(light.x, light.y, 3, light.x, light.y, light.radius);
+  var g = ctx.createRadialGradient(light.x, light.y, radius / 2, light.x, light.y, light.radius);
   g.addColorStop(0, light.color());
   g.addColorStop(1, light.ambientLightIntensity());
 
   ctx.beginPath();
-  ctx.arc(light.x, light.y, light.radius, 0, Math.PI*2, true);
+  ctx.arc(light.x, light.y, light.radius, 0, Math.PI * 2, true);
   ctx.fillStyle = g;
   ctx.closePath();
   ctx.fill();
@@ -97,7 +102,7 @@ function updateLights() {
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  for (var i=0; i<lights.length; i++) {
+  for (var i = 0; i < lights.length; i++) {
 
     var current = lights[i];
     current.intensityPathIndex = getNextIntensityPathIndex(current.intensityPathIndex);
@@ -122,7 +127,6 @@ function updateLights() {
         }
         else {
           current.interval += intervalGapCloser;
-          //console.log(current.interval);
         }
       }
     }
@@ -131,10 +135,10 @@ function updateLights() {
   }
 
   var now = new Date();
-  if ( ((now - last) > timeout) && lights[0].intensity() === 1) {
-   last = now;
-   clearInterval(lightTimer);
-   run();
+  if (((now - last) > timeout) && lights[0].intensity() === 1) {
+    last = now;
+    clearInterval(lightTimer);
+    run();
   }
 }
 
@@ -148,11 +152,10 @@ function getParam(paramName, defaultValue) {
   if (typeof p === "undefined") {
     p = defaultValue;
   }
-  if (p === "")
-  {
+  if (p === "") {
     p = defaultValue;
   }
-  console.log(paramName + ": "  + p + "; defaultValue: " + defaultValue);
+  console.log(paramName + ": " + p + "; defaultValue: " + defaultValue);
   return p;
 }
 
@@ -167,4 +170,11 @@ function getNextIntensityPathIndex(currentIndex) {
       : currentIndex + 1;
 
   return idx;
+}
+
+function canvasClick() {
+  //alert('click');
+  last = new Date();
+  clearInterval(lightTimer);
+  run();
 }
